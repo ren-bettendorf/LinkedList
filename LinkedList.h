377f5a660b13b700d06a1ea0cb3afbe9e746fc2d
Ren Bettendorf <ren@bettendorfapplications.com>
2015-12-28 14:36:10 -0800

    Fixed a bug in Node.cpp

--- LinkedList.h
+++ LinkedList.h
@@ -1,16 +1,25 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 #include "Node.h"
+#include <iostream>
 
 class LinkedList
 {
     public:
         LinkedList();
+        LinkedList(const LinkedList& list);
         virtual ~LinkedList();
+
+        bool isEmpty() const;
+        int getLength() const;
+        void insert(int index, const int& item);
+        void remove(int index);
+        void retrieve(int index, int& item) const;
     protected:
     private:
-        unsigned int size;
-        Node* header;
+        Node *find(int index) const;
+        int size;
+        Node *head;
 };
 
 #endif // LINKEDLIST_H
--- LinkedListTest.cpp
+++ LinkedListTest.cpp
@@ -7,7 +7,12 @@ namespace
 
     TEST(ScoreTestPerfect)
     {
-        std::cout << "Creating Node: " ;
+        LinkedList *list = new LinkedList();
+        for(int index = 1; index <= 10; index++)
+        {
+            std::cout << "Inserting node at start to position: " << index << std::endl;
+            list->insert(list->getLength() + 1 , index);
+        }
         CHECK_EQUAL(true, true);
     }
 }
